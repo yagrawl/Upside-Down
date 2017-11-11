45d17aa1b4ef94bd378ad12480a1e9597088f851
Yash Agrawal <yagrawl2@gmail.com>
2017-11-11 01:11:55 -0600

    basic stuff

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 
+function setup() {
+	const main_space = document.getElementById('main-space');
+	for(let i = 1; i <= 26; i++) {
+		let p = document.createElement('p');
+		p.className = 'letter';
+		p.id = String.fromCharCode(i+64);
+		p.innerHTML = String.fromCharCode(i+64);
+		if(i === 9 || i === 18) {
+			let brk = document.createElement('br');
+			main_space.appendChild(brk);
+		}
+		main_space.appendChild(p);
+	}
+}
+
 var i = 0;                     
 
 function main() {
-   let word = "Strange";  
-   let draw = document.getElementById("draw-space");         
-   setTimeout(function () {    
-      console.log('hello'); 
-      draw.innerHTML = word[i];         
-      i++;                     
-      if (i < word.length) {           
-         main();             
-      }                       
-   }, 1000)
+   	let word = "Strange"; 
+   	word = word.toUpperCase(); 
+   	setTimeout(function () {  
+   		let curr_letter = document.getElementById(word[i]);
+   		curr_letter.className = "letter-active";
+    	i++;                     
+    	if (i < word.length) {          
+    		main();     
+    	}                       
+   	}, 1000);
 }
